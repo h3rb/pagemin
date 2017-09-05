41ec60654f28beafe747b79e4391c82991d11693
h3rb <hgilliland@gmail.com>
2017-09-05 12:57:54 -0400

    Removed (minimal file content)

--- core/engines.php
+++ core/engines.php
@@ -1,4 +1,4 @@
 <?php
  include_once 'path.php';
- include_once SITE_ROOT.'/engines/HData.php';
- include_once SITE_ROOT.'/engines/Parsedown.php';
+ //include_once SITE_ROOT.'/engines/HData.php';
+ //include_once SITE_ROOT.'/engines/Parsedown.php';
